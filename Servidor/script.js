20cb3bd8a3c248364908ac0088f2ab7716aa8ac9
Yudi Aguena <yudi.aguena@gmail.com>
2023-07-23 15:45:24 -0300

    commit to main

--- Servidor/script.js
+++ Servidor/script.js
@@ -1,21 +1,5 @@
+const statusElement = document.getElementById('status');
 
-function verificarStatusServidor() {
-    // Simulando uma requisição para o servidor
-    setTimeout(function () {
-        // Suponha que o servidor tenha respondido com um status booleano (true = validado, false = não validado)
-        const statusDoServidor = Math.random() < 0.5; // Exemplo: 50% de chance de ser validado ou não
-
-        if (statusDoServidor) {
-            // Se o servidor respondeu "true", atualize o texto para "Validado"
-            document.getElementById("status").textContent = "Validado";
-        } else {
-            // Caso contrário, continue aguardando a validação
-            verificarStatusServidor();
-        }
-    }, 2000); // Intervalo de 2 segundos entre as verificações
+function mudarStatus() {
+  statusElement.textContent = 'Validado';
 }
-
-// Inicie a verificação do status do servidor quando a página estiver carregada
-document.addEventListener("DOMContentLoaded", function () {
-    verificarStatusServidor();
-});
